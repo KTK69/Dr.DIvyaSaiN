74adbcf8039b0bf1c3e0a713b893b39c72a45541
KTK69 <tanveerkrishna@gmail.com>
2026-05-19 21:15:09 +0530

    Add Google tag to site layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import FloatingActions from "@/components/ui/FloatingActions";
 import { SiteContentProvider } from "@/components/site/SiteContentProvider";
 import { getStoredSiteContent } from "@/lib/site-content-store";
+
+const GOOGLE_TAG_ID = "G-DF5TQCEYCB";
 
 export async function generateMetadata(): Promise<Metadata> {
   const { content } = await getStoredSiteContent();
@@ -45,6 +48,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_TAG_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GOOGLE_TAG_ID}');
+          `}
+        </Script>
         <script
           src="https://assets.calendly.com/assets/external/widget.js"
           async
